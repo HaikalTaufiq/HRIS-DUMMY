ab94a54f2d4ed13e3d68f2666d8df85277752760
HaikalTaufiq <134383454+HaikalTaufiq@users.noreply.github.com>
2025-12-16 13:50:21 +0700

    CORS

--- config/cors.php
+++ config/cors.php
@@ -11,18 +11,22 @@ return [
     'paths' => [
         'api/*',
         'sanctum/csrf-cookie',
-        'storage/*',
     ],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-	'https://103.161.188.102:443',
-	'https://103.161.188.102:444',
-	'http://103.161.188.102:80'
+        'https://103.161.188.102:443',
+        'https://103.161.188.102:444',
+        'http://103.161.188.102:80',
+        'http://localhost:64644',
+        'http://localhost:5173',
+        'https://yellow-frost-7be7.haikaltaufiq4.workers.dev/#/login'
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        'https://*.workers.dev',
+    ],
 
     'allowed_headers' => ['*'],
 
